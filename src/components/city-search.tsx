bb9af02da8459560376887e5e220f7ce3e3f71e2
Abhay Kumar <abhayk6190@gmail.com>
2025-05-28 20:17:48 +0530

    Second Commit

--- src/components/city-search.tsx
+++ src/components/city-search.tsx
@@ -48,11 +48,11 @@ const CitySearch = () => {
     <>
       <Button
         variant={"outline"}
-        className="relative w-full justify-start text-sm text-muted-foreground sm:pr-12 md:w-40 lg:w-64"
+        className="relative  justify-start text-sm text-muted-foreground sm:pr-12 sm:w-20 md:w-40 lg:w-64"
         onClick={() => setOpen(true)}
       >
         <Search className="mr-2 h-4 w-4" />
-        Search cities...
+        <span className="hidden md:inline md:visible">Search cities...</span>
       </Button>
       <CommandDialog open={open} onOpenChange={setOpen}>
         <CommandInput
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,12 +8,12 @@ const Header = () => {
     const isDark = theme ==="dark";
   return (
     <header className='sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur py-2 supports-[backdrop-filter]:bg-background/60'>
-        <div className='container mx-auto flex h-16 items-center justify-around md:justify-between px-4 flex-wrap'>
+        <div className='container mx-auto flex h-16 items-center justify-between px-4'>
             <Link to={"/"}>
             <img src= {isDark ? "logo-dark.png" : "logo-light.png"} alt="DriftCast Logo" className='h-18'/>
             </Link>
 
-            <div className='flex gap-4 '>
+            <div className='flex gap-4'>
                 {/* search */}
                 <CitySearch/>
                 {/* theme toggle */}
