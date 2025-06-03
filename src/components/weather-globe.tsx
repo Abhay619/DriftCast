7f38730d2eb36d0a8ba555f734f21b2c732b64a9
Abhay Kumar <abhayk6190@gmail.com>
2025-06-04 03:28:07 +0530

    Changes in weather-globe file to resolve issues

--- src/components/weather-globe.tsx
+++ src/components/weather-globe.tsx
@@ -1,7 +1,7 @@
 import type { WeatherData } from "@/api/types";
 import { Card, CardContent } from "./ui/card";
 import { useRef, useEffect } from "react";
-import Globe from "react-globe.gl";
+import Globe, { type GlobeMethods } from "react-globe.gl";
 import { useTheme } from "@/context/theme-provider";
 
 interface WeatherDataProps {
@@ -9,7 +9,7 @@ interface WeatherDataProps {
 }
 
 const WeatherGlobe = ({ data }: WeatherDataProps) => {
-  const globeRef = useRef();
+  const globeRef = useRef<GlobeMethods | undefined>(undefined);
   console.log(data.coord.lat);
 
   useEffect(() => {
@@ -19,26 +19,26 @@ const WeatherGlobe = ({ data }: WeatherDataProps) => {
         1000
       );
     }
-  }, []);
+  }, [data.coord.lat, data.coord.lon]);
   const { theme } = useTheme();
   const isDark = theme === "dark";
   return (
     <Card className="shadow-xl dark:border-zinc-800 border-zinc-300">
       <CardContent className="rounded-t-2xl overflow-hidden flex justify-center items-center">
-        <Globe
-          ref={globeRef}
-          width={800}
-          height={400}
-          globeImageUrl={
-            isDark
-              ? "//unpkg.com/three-globe/example/img/earth-dark.jpg"
-              : "//unpkg.com/three-globe/example/img/earth-day.jpg"
-          }
-          backgroundColor="rgba(0,0,0,0)"
-          pointsData={[{ lat: data.coord.lat, lng: data.coord.lon, size: 1 }]}
-          pointColor={() => "gray"} // Tailwind violet-500
-          pointAltitude={0.05}
-        />
+    <Globe
+      ref={globeRef}
+      width={800}
+      height={400}
+      globeImageUrl={
+        isDark
+          ? "//unpkg.com/three-globe/example/img/earth-dark.jpg"
+          : "//unpkg.com/three-globe/example/img/earth-day.jpg"
+      }
+      backgroundColor="rgba(0,0,0,0)"
+      pointsData={[{ lat: data.coord.lat, lng: data.coord.lon, size: 1 }]}
+      pointColor={() => "gray"} // Tailwind violet-500
+      pointAltitude={0.05}
+    />
       </CardContent>
     </Card>
   );
